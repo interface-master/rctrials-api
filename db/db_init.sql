142e2e364adf378c969dac7984a9ee541ceea551
Michael Vinogradov <interface.master@gmail.com>
2018-06-04 00:47:12 -0400

    adding table definition for tokens

--- db/db_init.sql
+++ db/db_init.sql
@@ -7,10 +7,17 @@ CREATE TABLE `users` (
 	`email` VARCHAR(100) NOT NULL,
 	`pass` VARCHAR(100) NOT NULL,
 	`name` VARCHAR(100) NOT NULL,
-	`role` ENUM('root','admin','user'),
+	`role` ENUM('root','admin','user') NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- INSERT INTO `users` (`id`,`salt`,`hash`,`email`,`pass`,`name`,`role`) VALUES
 -- 	( UUID(), '..salt..', '..hash..', 'interface.master@gmail.com', 'abc', 'Interface Master', 'root' ),
 -- 	( UUID(), '..salt..', '..hash..', 'john@smith.ca', 'Passw0rd', 'John Smith', 'admin' );
+
+CREATE TABLE `tokens` (
+	`uid` VARCHAR(36) NOT NULL,
+	`token` TEXT NOT NULL,
+	`expires` DATETIME NOT NULL,
+	PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
