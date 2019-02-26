062b5a1b6696197a010e7fb0142d9ac2b5815b3d
Michael Vinogradov <interface.master@gmail.com>
2019-02-26 15:27:12 -0500

    bugfixing

--- src/Classes/DatabaseManager.php
+++ src/Classes/DatabaseManager.php
@@ -468,7 +468,7 @@ class DatabaseManager {
 			'tid' => $tid
 		));
 		$trial = $stmt->fetch(\PDO::FETCH_OBJ);
-		if( !$trial->tid ) { return null; } // !! terminate if no trials match this user id !!
+		if( !$trial || !$trial->tid ) { return []; } // !! terminate if no trials match this user id !!
 		unset( $trial->uid );
 		// groups
 		$stmt = $this->dbh->prepare(
