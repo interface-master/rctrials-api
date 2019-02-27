00bbe31e77e921f1f130f0f13e329c3d17be1c2e
Michael Vinogradov <interface.master@gmail.com>
2019-02-26 21:27:09 -0500

    updating db test

--- src/info.php
+++ src/info.php
@@ -2,23 +2,18 @@
 	/**
 	 * TEST 1 : run `phpinfo` to make sure PHP, PDO, MySQL, etc, are all in working order
 	 */
-?>
 
-<?php
-	// echo phpinfo();
+	echo phpinfo();
 ?>
 
 <?php
 	/**
 	 * TEST 2 : check PHP+PDO+MySQL = sample data output
 	 */
-?>
-
-<?php
 
 	// config
 	$host = 'mysql';
-	$db   = 'mrct';
+	$db   = 'rctrials';
 	$user = 'root';
 	$pass = 'rooot';
 	$charset = 'utf8mb4';
@@ -33,13 +28,12 @@
 	$pdo = new PDO($dsn, $user, $pass, $opt);
 
 	// query
-	$stmt = $pdo->query('SELECT * FROM users');
+  echo "executing SQL: `DESCRIBE users`;<br/>\n";
+	$stmt = $pdo->query('DESCRIBE users');
 
 	// output
 	while ( $row = $stmt->fetch() ) {
-			echo "[" . $row['id'] . "]<br/>\n";
-			echo "[" . $row['salt'] . "]+[" . $row['pass'] . "] = [" . $row['hash'] . "]<br/>\n";
-			echo "[" . $row['name'] . "] = [" . $row['email'] . "] is [" . $row['role'] . "]<br/>\n";
+			var_dump( $row );
 			echo "&nbsp;<br/>\n";
 	}
 
