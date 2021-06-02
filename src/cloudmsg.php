fcd94afc86feec2a4792c905e50b229e177414c5
Michael Vinogradov <interface.master@gmail.com>
2021-06-01 22:23:43 -0400

    updating cloudmsg function to only msg every 3 days

--- src/cloudmsg.php
+++ src/cloudmsg.php
@@ -49,31 +49,40 @@ $sql = "SELECT `s`.`id` AS `subject`, `s`.`tid`, `s`.`group`, `f6e_token`,
                `v`.`groups` AS `survey_groups`,
                `v`.`interval` AS `survey_interval`,
                `v`.`frequency` AS `survey_frequency`,
-               `ans`.`answers_date`
+               `ans`.`answers_date`,
+               `log`.`sent_date`
           FROM `subjects` `s`
     INNER JOIN `surveys` `v`
-               ON (`s`.`tid` = `v`.`tid`
-                   AND `v`.`during` = 1
-                   AND FIND_IN_SET(`s`.`group`,
-                     SUBSTRING(`v`.`groups`, 2, LENGTH(`v`.`groups`)-2)
-                   ) <> 0
+            ON (`s`.`tid` = `v`.`tid`
+                 AND `v`.`during` = 1
+                 AND FIND_IN_SET(`s`.`group`,
+                   SUBSTRING(`v`.`groups`, 2, LENGTH(`v`.`groups`)-2)
+                 ) <> 0
                )
      LEFT JOIN (    SELECT `uid`, `tid`, `sid`, MAX(DATE(`timestamp`)) AS `answers_date`
                       FROM `answers`
                   GROUP BY `uid`, `tid`, `sid`
-                ) AS `ans`
-                ON (`s`.`id`=`ans`.`uid`
-                    AND `s`.`tid`=`ans`.`tid`
-                    AND `v`.`sid`=`ans`.`sid`
-                )
-          WHERE `f6e_token` IS NOT NULL
-            AND
-            (
-              DATEDIFF( NOW(), `ans`.`answers_date` ) > `v`.`interval`
-              OR
-              DATEDIFF( NOW(), `ans`.`answers_date` ) IS NULL
-            )
-    LIMIT 1
+               ) AS `ans`
+            ON (`s`.`id`=`ans`.`uid`
+                 AND `s`.`tid`=`ans`.`tid`
+                 AND `v`.`sid`=`ans`.`sid`
+               )
+     LEFT JOIN (    SELECT `uid`, `tid`, `sid`, MAX(DATE(`timestamp`)) AS `sent_date`
+                      FROM `f6e_logs`
+                  GROUP BY `uid`, `tid`, `sid`
+               ) AS `log`
+            ON (`s`.`id`=`log`.`uid`
+                 AND `s`.`tid`=`log`.`tid`
+                 AND `v`.`sid`=`log`.`sid`
+               )
+         WHERE `s`.`f6e_token` IS NOT NULL
+           AND (
+                 DATEDIFF( NOW(), `ans`.`answers_date` ) > `v`.`interval`
+                 OR
+                 DATEDIFF( NOW(), `ans`.`answers_date` ) IS NULL
+               )
+           AND ( DATEDIFF( NOW(), `log`.`sent_date` ) > 1 )
+    -- LIMIT 1 -- FOR DEBUGGING ONLY
     ;";
 
 $stmt = $dbh->prepare( $sql );
@@ -92,7 +101,8 @@ foreach( $tokens as $key => $token ) {
 
   // NOTE: overriding deviceToken to NOT SEND to ACTUAL USERS
   // TODO: remove in production after testing
-  $deviceToken = 'cl5aChADRhmyWBpwlc46-E:APA91bGcmRIBEzMyme7aLPREYxLsL2AvMGJ9jCeTZWNk0tdDBdHrfnBBLFW37gqYwmDbSppvBRu6yzlzZsPbXv-K7o_9Sbgry2BTGQZ8Kdiwj6w3OxYsUq2NMqZxNAgrzcl2wnBv-aoq'; //$token->f6e_token;
+  // $deviceToken = 'cl5aChADRhmyWBpwlc46-E:APA91bGcmRIBEzMyme7aLPREYxLsL2AvMGJ9jCeTZWNk0tdDBdHrfnBBLFW37gqYwmDbSppvBRu6yzlzZsPbXv-K7o_9Sbgry2BTGQZ8Kdiwj6w3OxYsUq2NMqZxNAgrzcl2wnBv-aoq'; //$token->f6e_token;
+  $deviceToken = $token->f6e_token;
   $msgTitle = 'Mehailo Survey';
   $msgBody = 'Please take 30 seconds (really, we timed it) to fill out a survey and help our research!';
   $notification = Notification::create( $msgTitle, $msgBody );
